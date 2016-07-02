7b32e238dd8e9ab19228c4dbd93f5996f944887c
Eska <eskil.nordland@gmail.com>
2016-07-02 06:19:28 +0200

    added test for Channel and Post-model

--- lolchan/lolchan_core/tests/test_models.py
+++ lolchan/lolchan_core/tests/test_models.py
@@ -1,1 +1,21 @@
-#from django.test import TestCase
+from django.test import TestCase
+from lolchan.lolchan_core.models import *
+from model_mommy import mommy
+from django.db.utils import IntegrityError
+
+
+class TestChannelModel(TestCase):
+
+    def test_unique_channel_name(self):
+        with self.assertRaisesMessage(IntegrityError, ''):
+            mommy.make('lolchan_core.Channel', name='Cats')
+            mommy.make('lolchan_core.Channel', name='Cats')
+
+
+class TestPostModel(TestCase):
+
+    def test_post_title_unique_to_channel(self):
+        testchannel = mommy.make('lolchan_core.Channel')
+        with self.assertRaisesMessage(IntegrityError, ''):
+            mommy.make('lolchan_core.Post', title='Funny cats', channel=testchannel)
+            mommy.make('lolchan_core.Post', title='Funny cats', channel=testchannel)
